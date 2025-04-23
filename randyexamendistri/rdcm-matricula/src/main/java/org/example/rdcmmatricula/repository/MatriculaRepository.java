68d029e2bad7e8ef110e5303e3d0a98e69376451
Randy236 <136346275+Randy236@users.noreply.github.com>
2025-04-23 18:08:56 -0500

    Delete randyexamendistri directory

--- randyexamendistri/rdcm-matricula/src/main/java/org/example/rdcmmatricula/repository/MatriculaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.rdcmmatricula.repository;
-
-import org.example.rdcmmatricula.entity.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Integer> {
-}
