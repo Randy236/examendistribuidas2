d12535e412c35b305cc8d6b49e16b79997110ea4
Randy236 <136346275+Randy236@users.noreply.github.com>
2025-04-23 17:28:56 -0500

    Add files via upload

---
+++ randyexamendistri/rdcm-matricula/src/main/java/org/example/rdcmmatricula/feing/EstudianteFeing.java
@@ -0,0 +1,13 @@
+package org.example.rdcmmatricula.feing;
+
+import org.example.rdcmmatricula.dato.Estudiante;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "rdcm-estudiante", path = "/estudiantes")
+public interface EstudianteFeing {
+    @GetMapping("/{id}")
+    ResponseEntity<Estudiante> obtenerPorId(@PathVariable Integer id);
+}
